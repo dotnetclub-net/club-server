f1073c0ae0a91ea3acbf3f9fd27d76850b6e34b5
Jijie Chen <jijie.chen@someplace.com>
2019-06-29 20:40:05 +0700

    增加了用于说明如何部署 efk 的提示

--- 2-efk/0-install.sh
+++ 2-efk/0-install.sh
@@ -3,6 +3,14 @@
 helm repo add akomljen-charts https://raw.githubusercontent.com/komljen/helm-charts/master/charts/
 
 helm install --name es-operator --namespace logging akomljen-charts/elasticsearch-operator
+
+
+# kubectl get CustomResourceDefinition
 helm install --name efk  --namespace logging  akomljen-charts/efk
 
 
+# kubectl delete ElasticsearchCluster/efk-cluster
+# kubectl apply -f ./1-patch-es.yaml
+# kubectl apply -f ./2-kibana-service.yaml
+# kubectl apply -f ./3-filebit-config.yaml
+# kubectl apply -f ./4-filebit-ds.yaml
